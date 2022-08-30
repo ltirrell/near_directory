ea69b066130500e448b051766a8f4fcb9d1b0dcc
Lee Tirrell <tirrell.le@gmail.com>
2022-08-30 18:28:53 +0800

    test api key

--- 🌆_NEAR_Directory.py
+++ 🌆_NEAR_Directory.py
@@ -1,5 +1,12 @@
 import streamlit as st
+import os
 
 st.set_page_config(page_title="NEAR Directory - Your guide to the City of NEAR", page_icon="🌆", layout="wide")
 st.title("NEAR Directory")
 st.caption("Your guide to the City of NEAR")
+
+shroomdk_key = os.getenv("SHROOMDK_KEY")
+figment_key = os.getenv("FIGMENT_API_KEY")
+
+st.write(shroomdk_key[:5])
+st.write(figment_key[:5])

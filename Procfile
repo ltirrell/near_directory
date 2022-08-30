e232d62a37c572f5acc9d41423e3673d7d1492e6
Lee Tirrell <tirrell.le@gmail.com>
2022-08-30 15:01:56 +0800

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: sh setup.sh && streamlit run 🌆_NEAR_Directory.py
---
+++ 🌆_NEAR_Directory.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="NEAR Directory - Your guide to the City of NEAR", page_icon="🌆", layout="wide")
+st.title("NEAR Directory")
+st.caption("Your guide to the City of NEAR")
